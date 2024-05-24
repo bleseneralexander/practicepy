85ddea44e227221dc145b632bb0122eabf3feeea
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-05-24 13:46:12 -0500

    Create prac.py

---
+++ prac.py
@@ -0,0 +1,17 @@
+def function()
+outlet = bytearray(4),
+outlet_bytes = bytes(2),
+outlet_range = range(4),
+outlet_comp = complex(10A),
+break;
+def function2()
+outlet_bytes() = window.Uti,
+outlet_bytes[0] = window.Uti[0],
+outlet_range(max()) = screen.length,
+outlet_range(min()) = screen.width,
+outlet_comp = id.string,
+break;
+for (outlet_comp = 1; outlet_comp < 5; outlet_comp++)
+{
+string + window.Uti[0] == outlet
+}
